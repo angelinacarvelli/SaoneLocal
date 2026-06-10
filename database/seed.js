711f30f27a95f30af744fcb46957f7e32739bb72
student <student@chalon21.local>
2026-06-10 11:01:02 +0200

    Fix(seed): change name to db

--- database/seed.js
+++ database/seed.js
@@ -7,7 +7,7 @@ dotenv.config()
 const client = new Client({
 user: process.env.POSTGRES_USER,
 host: process.env.POSTGRES_HOST,
-database: process.env.POSTGRES_NAME,
+database: process.env.POSTGRES_DB,
 password: process.env.POSTGRES_PASSWORD,
 port: process.env.POSTGRES_PORT,
 })
