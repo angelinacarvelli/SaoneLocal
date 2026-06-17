b4ff9dfb1ab3c64c8e856eee375d712e6d3379ed
Angelinacarvelli <angelinacarvellipro@gmail.com>
2026-06-17 11:10:09 +0200

    fix(components): fix components, it's now in JSX and react

--- SaoneLocal/src/App.jsx
+++ SaoneLocal/src/App.jsx
@@ -1,10 +1,15 @@
-import Gallery from './components/Gallery.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Navbar from './components/Navbar.jsx';
+import Card from './components/Card.jsx';
 
 export default function App() {
   return (
     <main>
       <Header />
       <Footer />
+      <Navbar />
+      <Card />
     </main>
   );
 }
