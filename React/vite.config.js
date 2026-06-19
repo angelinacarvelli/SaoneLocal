b0b565886e6747e2b875c6f9b73863bdae69d2c6
Angelinacarvelli <angelinacarvellipro@gmail.com>
2026-06-19 09:45:54 +0200

    fix(API): fix path to show frontend

--- React/vite.config.js
+++ React/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
-    port: 3000, // On change le port vers 3000 pour éviter les conflits
-    hmr: {
-      overlay: false, // Désactive la couche d'affichage des erreurs qui peut masquer ton site
-    },
+    port: 3000,
+    strictPort: true,
   },
 })
